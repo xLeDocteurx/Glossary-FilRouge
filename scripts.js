439fd2f7cab578cd0c899885d61457ea06b302da
xLeDocteurx <lenoirc.fr@gmail.com>
2018-05-18 22:40:51 +0200

    2hours later ... the book note is opening... BOUYA

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+
+    $("#open").click(function () {
+        console.log(`$(this).removeClass("frontc");`);
+        $("#page-index").removeClass("frontc");
+    });
+});
