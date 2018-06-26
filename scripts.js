d5337ee81d2401abe94fc571527e0ecd5c9a0cbc
xLeDocteurx <lenoirc.fr@gmail.com>
2018-06-26 13:48:29 +0200

    refresh and fixe of some layout problems. I separated the index in 2 parts. The closed version and the opened one

--- scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-
-    $("#open").click(function () {
-        console.log(`$(this).removeClass("frontc");`);
-        $("#page-index").removeClass("frontc");
-    });
-});
